fc7c641c327463a625871f83174d78047e3e8c79
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-06-18 04:36:43 -0300

    Fix: update structure, add a use very api translate and update reponsive design

--- src/script.js
+++ src/script.js
@@ -20,3 +20,33 @@ const toggle = document.querySelector(".dark-mode-toggle");
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 });
+
+//Português
+$(function() {
+    $(".br").click(function() {
+        $(".language-selected").text("pt-BR");
+        $(".language-selected").removeClass("change-en");
+        $(".language-selected").removeClass("change-es");
+        $(".language-selected").addClass("change-br");
+    });
+});
+
+//English
+$(function() {
+    $(".en").click(function() {
+        $(".language-selected").text("en");
+        $(".language-selected").removeClass("change-es");
+        $(".language-selected").removeClass("change-br");
+        $(".language-selected").addClass("change-en");
+    });
+});
+
+//Espanõl
+$(function() {
+    $(".es").click(function() {
+        $(".language-selected").text("es");
+        $(".language-selected").removeClass("change-br");
+        $(".language-selected").removeClass("change-en");
+        $(".language-selected").addClass("change-es");
+    });
+});
--- src/services/googleTranslate.js
+++ src/services/googleTranslate.js
@@ -1,10 +1,31 @@
+var comboGoogleTradutor = null; //Varialvel global
+
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    {
-      pageLanguage: 'pt-br', 
-      layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
-    }, 
-    'google_translate_element'
-  );
+  new google.translate.TranslateElement({
+    pageLanguage: 'pt',
+    includedLanguages: 'en,es',
+    layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+  }, 'google_translate_element');
+
+  comboGoogleTradutor = document.getElementById("google_translate_element").querySelector(".goog-te-combo");
+}
+
+function changeEvent(el) {
+  if (el.fireEvent) {
+    el.fireEvent('onchange');
+  } else {
+    var evObj = document.createEvent("HTMLEvents");
+
+    evObj.initEvent("change", false, true);
+    el.dispatchEvent(evObj);
+  }
+}
+
+function trocarIdioma(sigla) {
+  if (comboGoogleTradutor) {
+    comboGoogleTradutor.value = sigla;
+    //Dispara a troca
+    changeEvent(comboGoogleTradutor); 
+  }
 }
 
